2b95f96ac15023710ffe2abecb07b26b4daa9158
pedrohoppe88 <pedrohenriquehoppe6@gmail.com>
2022-11-16 19:40:19 -0300

    Iugyirf

---
+++ geral/6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+      int main() {
+            float n;
+            float resultado;
+
+            printf("\nDigite um valor: ");
+            scanf("%f", &n);
+
+            resultado = sqrt(n);
+
+      }
